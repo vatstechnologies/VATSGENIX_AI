60546561400632262b73fa03f982ddc6552a9b35
Anurag Vats <anuragvats2903@outlook.com>
2025-02-07 13:36:26 +0530

    Update app.py

--- VATSGENIX_AI/app.py
+++ VATSGENIX_AI/app.py
@@ -1,6 +1,7 @@
 import os
 import streamlit as st
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")  # No need for .env
 import google.generativeai as gen_ai
 
 # Load environment variables from .env file
